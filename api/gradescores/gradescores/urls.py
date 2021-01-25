43e327dc4cd188f8b13f4f2cdc045b0e9d6834fa
dxghost <ghost@dx.dx>
2021-01-26 03:22:48 +0330

    Add PersonViewset

--- api/gradescores/gradescores/urls.py
+++ api/gradescores/gradescores/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers, serializers, viewsets
+from main.views import *
+
+router = routers.DefaultRouter()
+router.register(r'people', PersonViewset)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
+
 ]
--- api/gradescores/main/views.py
+++ api/gradescores/main/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import permissions,viewsets
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+class PersonViewset(viewsets.ModelViewSet):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+
